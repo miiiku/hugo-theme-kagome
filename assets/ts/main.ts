979a2e35c1e852251dae131299c810c922956d39
Sukoshi <guanquanhong@163.com>
2021-06-18 17:35:19 +0800

    feat(js,style): 修改js部分代码加载逻辑，css优化

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -1,47 +1,51 @@
-window.addEventListener('load', () => {
-  /** moble click toggle menu */
-  const mobileMenuBtn:Element = document.querySelector('.header-nav--btn')
-  mobileMenuBtn.addEventListener('click', function () {
-    this.classList.toggle('open')
-  })
-
-  /** theme change click */
+(function () {
+  
   const currentTheme:string  = window.localStorage.getItem('theme') || ''
   if (currentTheme && currentTheme !== 'auto') {
     switchTheme(currentTheme)
   }
-  const themeLightBtn = document.querySelector('#theme-light')
-  const themeDarkBtn  = document.querySelector('#theme-dark')
-  const themeAuto     = document.querySelector('#theme-auto')
-  themeLightBtn.addEventListener('click', () => switchTheme('light'))
-  themeDarkBtn.addEventListener('click', () => switchTheme('dark'))
-  themeAuto.addEventListener('click', () => switchTheme('auto'))
 
-  /** background image lazy */
-  const lazyBackgrounds = querySelectorArrs('.background-image-lazy')
-  let lazyBackgroundsCount = lazyBackgrounds.length
-  if (lazyBackgroundsCount > 0) {
-    let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function({ isIntersecting, target }) {
-        if (isIntersecting) {
-          let img = target.dataset.img
-          if (img) {
-            target.style.backgroundImage = `url(${img})`
+  window.addEventListener('load', () => {
+    /** moble click toggle menu */
+    const mobileMenuBtn:Element = document.querySelector('.header-nav--btn')
+    mobileMenuBtn.addEventListener('click', function () {
+      this.classList.toggle('open')
+    })
+
+    /** theme change click */
+    const themeLightBtn = document.querySelector('#theme-light')
+    const themeDarkBtn  = document.querySelector('#theme-dark')
+    const themeAuto     = document.querySelector('#theme-auto')
+    themeLightBtn.addEventListener('click', () => switchTheme('light'))
+    themeDarkBtn.addEventListener('click', () => switchTheme('dark'))
+    themeAuto.addEventListener('click', () => switchTheme('auto'))
+
+    /** background image lazy */
+    const lazyBackgrounds = querySelectorArrs('.background-image-lazy')
+    let lazyBackgroundsCount = lazyBackgrounds.length
+    if (lazyBackgroundsCount > 0) {
+      let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function({ isIntersecting, target }) {
+          if (isIntersecting) {
+            let img = target.dataset.img
+            if (img) {
+              target.style.backgroundImage = `url(${img})`
+            }
+            lazyBackgroundObserver.unobserve(target)
+            lazyBackgroundsCount --
           }
-          lazyBackgroundObserver.unobserve(target)
-          lazyBackgroundsCount --
-        }
-        if (lazyBackgroundsCount <= 0) {
-          lazyBackgroundObserver.disconnect()
-        }
+          if (lazyBackgroundsCount <= 0) {
+            lazyBackgroundObserver.disconnect()
+          }
+        })
       })
-    })
 
-    lazyBackgrounds.forEach(function(lazyBackground) {
-      lazyBackgroundObserver.observe(lazyBackground)
-    })
-  }
-})
+      lazyBackgrounds.forEach(function(lazyBackground) {
+        lazyBackgroundObserver.observe(lazyBackground)
+      })
+    }
+  });
+})();
 
 function querySelectorArrs (selector):Array<Element> {
   return Array.from(document.querySelectorAll(selector))
