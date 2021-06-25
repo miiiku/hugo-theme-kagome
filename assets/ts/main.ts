17cab7c12191adf0b612209f610ae7494ccc5b28
Sukoshi <guanquanhong@163.com>
2021-06-25 21:30:36 +0800

    feat(aplayer&dplayer): 优化aplayer，dplayer相关代码

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -44,14 +44,57 @@
         lazyBackgroundObserver.observe(lazyBackground)
       })
     }
+
+    /** aplayer init */
+    aplayerInit()
+    /** dplayer init */
+    dplayerInit()
   });
 })();
 
-function querySelectorArrs (selector):Array<Element> {
+function toCamel(str:string) {
+  const arrs = str.split('-')
+  if (arrs.length === 1) return arrs[0]
+  return arrs.reduce((accumulator:string, currentValue:string) => {
+    return accumulator + currentValue.toLowerCase().replace(/( |^)[a-z]/g, v => v.toUpperCase())
+  })
+}
+
+/**
+ * 处理aplayer || dplayer 参数
+ * @param el aplayer || dplayer dom
+ * @returns 配置项参数
+ */
+function formatAttr(el:Element): object {
+  const config = {}
+  const numberList = ['lrcType']
+  const boolMap = new Map([
+    ['true', true],
+    ['false', false],
+  ])
+  const attrs = el.getAttributeNames().filter(key => key.startsWith('config-'))
+  attrs.forEach(attr => {
+    const key = toCamel(attr.replace('config-', ''))
+    const value = el.getAttribute(attr)
+    const toBool = boolMap.get(value)
+    
+    if (toBool !== undefined) {             /** 处理bool值 */
+      config[key] = toBool
+    } else if (numberList.includes(key)) {  /** 处理number值 */
+      config[key] = parseInt(value)
+    } else {                                /** string */
+      config[key] = value
+    }
+  })
+  console.log(config)
+  return config
+}
+
+function querySelectorArrs (selector:string):Array<Element> {
   return Array.from(document.querySelectorAll(selector))
 }
 
-function switchTheme (theme) {
+function switchTheme (theme:string) {
   const rootDom:Element = document.documentElement
   if (theme === 'auto') {
     rootDom.classList.remove('theme-dark')
@@ -66,4 +109,26 @@ function switchTheme (theme) {
     rootDom.classList.add('theme-light')
   }
   window.localStorage.setItem('theme', theme)
+}
+
+function aplayerInit () {
+  const aplayers = querySelectorArrs('.aplayer-box')
+  if (aplayers.length && APlayer) {
+    aplayers.forEach(el => {
+      const params = { container: el, audio: { ...el.dataset } }
+      const config = formatAttr(el)
+      new APlayer(Object.assign({}, config, params))
+    })
+  }
+}
+
+function dplayerInit () {
+  const dplayers = querySelectorArrs('.dplayer-box')
+  if (dplayers.length && DPlayer) {
+    dplayers.forEach(el => {
+      const params = { container: el, video: { ...el.dataset } }
+      const config = formatAttr(el)
+      new DPlayer(Object.assign({}, config, params))
+    })
+  }
 }
